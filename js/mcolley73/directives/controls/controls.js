8b48ddf2b3df5c0f02f5698f0ea270039ab0cb16
Mark Colley <mcolley73@gmail.com>
2016-04-10 00:31:10 -0400

    Resets the sample world dropdown when clearing.

--- js/mcolley73/directives/controls/controls.js
+++ js/mcolley73/directives/controls/controls.js
@@ -1,7 +1,7 @@
 gameOfLifeApp.directive('golControls', ['$log', 'gameService', 'gameDataService', 'sampleWorldService', function($log, gameService, gameDataService, sampleWorldService){
-	
+
 	var controller = ['$log', '$scope', 'gameService', 'gameDataService', 'sampleWorldService', function($log, $scope, gameService, gameDataService, sampleWorldService){
-		
+
 		$scope.gameService = gameService;
 		$scope.gameDataService = gameDataService;
 		$scope.sampleWorldService = sampleWorldService;
@@ -9,62 +9,63 @@ gameOfLifeApp.directive('golControls', ['$log', 'gameService', 'gameDataService'
 		$scope.startGame = function(){
 			gameService.startGame();
 		};
-		
+
 		$scope.pauseGame = function(){
 			gameService.stopGame();
 		};
-		
+
 		$scope.stepGame = function(){
 			gameService.stepGame();
 		};
-		
+
 		$scope.previewGeneration = function(){
 			gameService.previewGeneration();
 		};
 		$scope.removePreview = function(){
 			gameService.removePreview();
 		};
-		
+
 		$scope.newGame = function(){
 			gameService.newGame();
 		}
-		
+
 		$scope.clear = function(){
 			gameService.clear();
+			gameDataService.game.selectedSample = "";
 		}
-		
+
 		$scope.pauseAndResume = function(){
 			if(gameService.isRunning()){
 				gameService.stopGame();
 				gameService.startGame();
 			}
 		}
-		
+
 		$scope.rebuildWorld = function(){
 			gameService.rebuild();
 		}
-		
+
 		$scope.updateSampleWorld = function(){
 			if(gameDataService.game.selectedSample == null){
 				return;
 			}
 			gameService.createSample(gameDataService.game.selectedSample);
 		}
-		
+
 		$scope.generateJson = function(){
 			gameService.generateJson();
 		}
-		
+
 		$scope.gestationChange = function(){
     		$scope.pauseAndResume();
 		}
-		
+
 	}];
-	
+
 	return {
 		controller: controller,
 		model: '=',
 		templateUrl: '/GameOfLife/js/mcolley73/directives/controls/controls.html'
 	};
-	
+
 }]);
